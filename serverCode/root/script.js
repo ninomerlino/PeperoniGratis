e2050b0323b7e8c02db7ada48c7ad07563e910a6
ninomerlino <lorenzomerlino9@gmail.com>
2023-03-26 13:53:35 +0200

    Changed file to work with influxDB

--- serverCode/root/script.js
+++
@@ -1,1 +0,0 @@
-scrpit missing
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,76 @@
 #include "util.h"
 #include "battery.h"
-#include "httposter.h"
 #include "credentials.h"
 #include "logger.h"
-#include "payload.h"
+#include <InfluxDbClient.h>
+
 
 Battery battery;
-Sender sender;
 Logger logger(&Serial, DEBUG);
-Payload payload;
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+
+void connectToWifi(uint64_t timeout = 10000){
+  uint64_t start = millis();
+  WiFi.begin(SSID, PASSWORD);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+    if(millis() - start > timeout){
+      logger.log("Wifi connection timeout", ERROR);
+      return;
+    }
+  }
+  logger.log(WiFi.localIP(),INFO);
+}
+
+void connectToInfluxDB(uint64_t timeout = 10000){
+  uint64_t start = millis();
+  while(!client.validateConnection()){
+    delay(500);
+    Serial.print(".");
+    if(millis() - start > timeout){
+      logger.log("InfluxDB connection timeout", ERROR);
+      logger.log(client.getLastErrorMessage(), ERROR);
+      return;
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  sender = Sender(SSID, PASSWORD, SERVER_ADDRESS, SERVER_PORT);
   battery = Battery(BATTERY_PIN);
+
+  logger.log("Connecting to Wifi",INFO);
+  connectToWifi();
+
+  logger.log("Setting time",INFO);
+	timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
+  logger.log("Connecting to InfluxDB",INFO);
+  connectToInfluxDB();
+
+  logger.log("Setup completed",INFO);
 }
 
 void loop(){
-  payload.add_field("battery", battery.getCharge());
-  sender.sendPostRequest("/update", payload.to_string());
+  Point point("Serra");
+  point.addField("battery", battery.getCharge());
+
+  if(!WiFi.isConnected()){
+    logger.log("Wifi connection lost", ERROR);
+    connectToWifi();
+    return;
+  }
+  if(!client.isConnected()){
+    logger.log("InfluxDB connection lost", ERROR);
+    connectToInfluxDB();
+    return;
+  }
+
+  if (!client.writePoint(point)) {
+    logger.log("InfluxDB write failed: ", ERROR);
+    logger.log(client.getLastErrorMessage(), ERROR);
+
+  }
   delay(1000);
-  //clear after sending
-  payload.clear();
 }
