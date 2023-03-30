fe562f3100d945190d4cacef3753ebacf61dd20c
ninomerlino <lorenzomerlino9@gmail.com>
2023-03-30 14:37:51 +0200

    one single file for both webapp

--- serverCode/server.py
+++ serverCode/server.py
@@ -3,26 +3,21 @@ from fastapi.responses import HTMLResponse, Response, FileResponse
 from fastapi.staticfiles import StaticFiles
 from datetime import datetime
 import uvicorn
-
 from store import Store, Record
+from threading import Thread
 
 
-app = FastAPI()
+webServer = FastAPI()
+arduinoServer = FastAPI()
 store = Store("./store.csv")
-app.mount("/static", StaticFiles(directory="./client/static"), name="static")
+webServer.mount("/static", StaticFiles(directory="./client/static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+@webServer.get("/", response_class=HTMLResponse)
 async def read_root():
     with open("./client/index.html") as f:
         return HTMLResponse(content=f.read(), status_code=200)
 
-@app.post("/api/write")
-async def update(record: Record):
-    store.write(record)
-    store.save()
-    return Response(status_code=200)
-
-@app.get("/api/read")
+@webServer.get("/api/read")
 async def read(from_time: str | None = None, to_time: str | None = None):
     try:
         from_time = datetime.fromisoformat(from_time.replace('Z','+00:00')) if from_time is not None else None
@@ -31,9 +26,20 @@ async def read(from_time: str | None = None, to_time: str | None = None):
         return Response(content="Invalid date format", status_code=400)
     return store.read(from_time, to_time)
 
-@app.get("/api/read_dump")
+@webServer.get("/api/read_dump")
 async def read_dump():
     return FileResponse("./store.csv", media_type="text/csv", filename="store.csv")
 
+@arduinoServer.post("/api/write")
+async def update(record: Record):
+    store.write(record)
+    store.save()
+    return Response(content="OK", status_code=200)
 
-uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
+if __name__ == "__main__":
+    webThread = Thread(target=uvicorn.run,kwargs={"app":webServer,"port": 8001,"host":"0.0.0.0"})
+    arduinoThread = Thread(target=uvicorn.run,kwargs={"app":arduinoServer,"port": 8000,"host":"0.0.0.0"})
+    webThread.start()
+    arduinoThread.start()
+    webThread.join()
+    arduinoThread.join()
