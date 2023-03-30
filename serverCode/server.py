d423037130757e8042f1257ecd7e495332b1d156
ninomerlino <lorenzomerlino9@gmail.com>
2023-03-30 22:51:43 +0200

    something malicious is brewing

--- serverCode/server.py
+++ serverCode/server.py
@@ -20,21 +20,25 @@ async def read_root():
 @webServer.get("/api/read")
 async def read(from_time: str | None = None, to_time: str | None = None):
     try:
-        from_time = datetime.fromisoformat(from_time.replace('Z','+00:00')) if from_time is not None else None
-        to_time = datetime.fromisoformat(to_time.replace('Z','+00:00')) if to_time is not None else None
+        from_time = datetime.fromisoformat(from_time.replace('Z','+00:00')) if from_time is not None else None # type: ignore
+        to_time = datetime.fromisoformat(to_time.replace('Z','+00:00')) if to_time is not None else None # type: ignore
     except ValueError as e:
         return Response(content="Invalid date format", status_code=400)
-    return store.read(from_time, to_time)
+    return store.read(from_time, to_time) # type: ignore
 
 @webServer.get("/api/read_dump")
 async def read_dump():
     return FileResponse("./store.csv", media_type="text/csv", filename="store.csv")
+
+#------------------Arduino server------------------
 
 @arduinoServer.post("/api/write")
 async def update(record: Record):
     store.write(record)
     store.save()
     return Response(content="OK", status_code=200)
+
+#--------------------------------------------------
 
 if __name__ == "__main__":
     webThread = Thread(target=uvicorn.run,kwargs={"app":webServer,"port": 8001,"host":"0.0.0.0"})
